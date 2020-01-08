e9e649eecc9d8dd0db56b7e0ebe9a6703a1fb69c
iipshoifuddin <57024333+iipshoifuddin@users.noreply.github.com>
2020-01-08 23:20:49 +0700

    Update HomeScreen.js

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -64,32 +64,4 @@ const styles = StyleSheet.create({
   },
 });
 
-import {Button, View} from 'react-native';
-import {DARK_GREEN} from 'react-native-dotenv';
-
-class HomeScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Welcome to the app!',
-  };
-
-  render() {
-    return (
-      <View>
-        <Button title={DARK_GREEN} onPress={this._showMoreApp} />
-        <View style={{marginBottom: 30}} />
-        <Button title="Actually, sign me out :)" onPress={this._signOutAsync} />
-      </View>
-    );
-  }
-
-  _showMoreApp = () => {
-    this.props.navigation.navigate('Profile');
-  };
-
-  _signOutAsync = async () => {
-    // await AsyncStorage.clear();
-    this.props.navigation.navigate('Auth');
-  };
-}
-
 export default HomeScreen;
